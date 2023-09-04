4286dd31caed1f04afaae48d81461a74054dbcc3
Радченко Катерина <ketrinradchenko@gmail.com>
2023-09-04 19:12:59 +0300

    Add project files.

---
+++ dz_Threads2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace dz_Threads2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.textBoxWord = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Показать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(37, 41);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(182, 20);
+            this.textBoxPath.TabIndex = 2;
+            // 
+            // textBoxWord
+            // 
+            this.textBoxWord.Location = new System.Drawing.Point(37, 91);
+            this.textBoxWord.Name = "textBoxWord";
+            this.textBoxWord.Size = new System.Drawing.Size(182, 20);
+            this.textBoxWord.TabIndex = 3;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(144, 182);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(13, 13);
+            this.labelResult.TabIndex = 4;
+            this.labelResult.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "раз(а).";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Слово встречается";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 214);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.textBoxWord);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.TextBox textBoxWord;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ dz_Threads2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace dz_Threads2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Введите путь к файлу";
+            label2.Text = "Введите искомое слово";
+        }
+
+        struct MyStruct  // структура для передачи в делегат с параметром для потока
+        {
+            public string filePath;       // путь к файлу
+            public string wordForSearch;  // искомое слово
+            public MyStruct(string f, string w)
+            {
+                filePath = f;
+                wordForSearch = w;
+            }
+        }
+
+        public void SearchWord(object obj)
+        {
+            MyStruct myStruct = (MyStruct)obj;
+
+            string filePath = myStruct.filePath;
+            string wordForSearch = myStruct.wordForSearch;
+
+            int count = 0;
+            StringBuilder sb = new StringBuilder();  // создаем строку
+            using (StreamReader reader = new StreamReader(filePath))  // считываем файл
+            {
+                // присоединяем к созданной строке текст из файла
+                sb.Append(reader.ReadToEnd());
+                // текст из файла с пимощью метода Split преобразуем в массив слов
+                string[] s = sb.ToString().Split(new char[] { ' ', '.', ',', '!', '?', ':', ';', '\n'});
+                foreach (var item in s)
+                {
+                    if (item == wordForSearch)
+                        count++;
+                }
+            }
+            labelResult.Text = count.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // создаем и инициализируем объект структуры
+            MyStruct myStruct = new MyStruct(textBoxPath.Text, textBoxWord.Text);
+            // создаем поток, передаем делегат, инициализированный методом SearchWord
+            Thread thread = new Thread(new ParameterizedThreadStart(SearchWord));
+            thread.IsBackground = true;
+            // запускаем поток и в параметры передаем объект структуры
+            thread.Start(myStruct);
+        }
+    }
+}
